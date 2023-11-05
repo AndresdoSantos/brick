98ed01e6eb939253cd8a2cdacdcb053b13ca5861
Desenvolvimento <desenvolvimento@zaal.com.br>
2023-11-05 17:21:06 -0300

    feat(.husky): install husky and prettier

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,24 @@
 import { ActivityIndicator, Text, View } from 'react-native'
 import { white } from 'tailwindcss/colors'
 
-import { useFonts } from '../hooks/use-fonts'
+import { useFonts } from 'hooks/use-fonts'
 
 export default function Redirect() {
   const isLoaded = useFonts()
 
   if (!isLoaded) {
-    return <ActivityIndicator size="small" color={white} />
+    return (
+      <View className="flex-1 items-center justify-center bg-zinc-900">
+        <ActivityIndicator size="small" color={white} />
+      </View>
+    )
   }
 
   return (
     <View className="flex-1 items-center justify-center bg-zinc-900">
-      <Text>This is a Expo Router with Tailwindcss</Text>
+      <Text className="text-white font-400">
+        This is a Expo Router with Tailwindcss
+      </Text>
     </View>
   )
 }
--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,19 @@ module.exports = function (api) {
   api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['nativewind/babel', 'expo-router/babel'],
+    plugins: [
+      'nativewind/babel',
+      'expo-router/babel',
+      [
+        'module-resolver',
+        {
+          alias: {
+            app: './app',
+            components: './components',
+            hooks: './hooks',
+          },
+        },
+      ],
+    ],
   }
 }
