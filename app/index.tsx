cbf414a55d126803111a8661350516f122d8dccb
Desenvolvimento <desenvolvimento@zaal.com.br>
2023-11-05 15:46:11 -0300

    feat(app/index): nativewind, eslint & router

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function Redirect() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>This is a Expo Router with Tailwindcss</Text>
+    </View>
+  )
+}
