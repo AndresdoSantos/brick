8a3800d2a67de6179b7213fe06ccc507e866363a
Desenvolvimento <desenvolvimento@zaal.com.br>
2023-11-07 17:51:53 -0300

    test(__mocks__): add svg mock

--- jest.config.cjs
+++ jest.config.cjs
@@ -7,6 +7,7 @@ module.exports = {
   },
   moduleNameMapper: {
     '^hooks/(.*)$': '<rootDir>/hooks/$1',
+    '\\.svg': '<rootDir>/__mocks__/svg-mocks.js',
   },
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
